2dcf6e1a0eacfdd2ae134aa499ccc73619aa7bd2
Federico <vgfede@gmail.com>
2023-05-17 23:50:20 +0200

    ast update

--- lib/ast.ml
+++ lib/ast.ml
@@ -43,13 +43,11 @@ and ctype =
 [@@deriving show]
 
 
-
-type qname = 
-  | QIndex of int
-  | Var of var
+type access = access_node annotated_node
+and access_node =
+  | AccessVar of var
+  | AccessQBit of int
 [@@deriving show]
-type qnamelist = QNameList of qname list [@@deriving show]
-
 
 type expr = expr_node annotated_node
 and expr_node = 
@@ -57,10 +55,8 @@ and expr_node =
   | UnaryOp of uop * expr 
   | ILiteral of int
   | BLiteral of bool
-  | AccessVar of var
-  | AccessQNameList of qnamelist
+  | Access of access
 [@@deriving show]
-
 
 (*
   Types definition forms a cycle:
@@ -69,11 +65,11 @@ and expr_node =
 type seq = seq_node annotated_node
 and seq_node = 
   | Tau of par
-  | Measure of qnamelist * var * par
-  | QOp of qop * qnamelist * par
+  | Measure of access list * var * par
+  | QOp of qop * access list * par
   | Recv of chan * var * par
   | Send of chan * expr
-  | Discard of qnamelist
+  | Discard of access list
 [@@deriving show]
 
 and choice = choice_node annotated_node
@@ -92,3 +88,15 @@ type restr = restr_node annotated_node
 and restr_node = Restr of chan list [@@deriving show]
 
 type program = Prog of par_top * restr [@@deriving show]
+
+(*
+discard = Discard(AccessQBit(0))
+ifthenelse = 
+measurement = Measure([AccessQBit(0)], VarName("x"), ParTopLevel(Par([Choice([Seq(ifthenelse)]), Choice([Seq(discard)])]))))
+veroverissimo = QOp(H, [AccessQBit(0)], ParTopLevel(Par([Choice([Seq(measurement)])]))))
+
+Meas().Parall()
+
+Prog((Par([Choice([veroverissimo])]), Restr([])))
+
+*)
