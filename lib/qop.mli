e8a153a027891e2c98ed3280658ff258edd04fa1
Innocenzo <innocenzofulginiti@gmail.com>
2023-06-19 22:25:38 +0200

    minor changes qop.mli

--- lib/qop.mli
+++ lib/qop.mli
@@ -6,19 +6,6 @@ exception QuantumException of string
 
 type measurement_result = {quantum_state: t list; value: int list; probability: float}
 
-(* Utility functions *)
-val make_id : Mat.mat
-
-val list2vec : t list -> Mat.mat
-
-val vec2list : Mat.mat -> t list 
-
-val step_forwards : Mat.mat -> int -> Mat.mat
-
-val step_backwards : Mat.mat -> int -> Mat.mat
-
-val generate_cases : int -> int list -> (int * int) list list
-
 (* Quantum operations *)
 val qop_h : t list -> int -> t list
 
